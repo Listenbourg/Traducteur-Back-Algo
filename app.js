006b8639db48ec6c519f3fe0afc5142108599f47
Justin Milhomme <justin.milhomme@omnibook.com>
2022-11-02 22:02:27 +0100

    cors

--- app.js
+++ app.js
@@ -13,16 +13,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "http://localhost:3001",
-      "http://51.210.104.99:1658",
-      "*",
-    ],
-  })
-);
+app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
