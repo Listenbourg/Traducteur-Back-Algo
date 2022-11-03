3373196e94dc7f2d0ff409e5b161d4d6a6d54416
Just1bridou <45208166+Just1bridou@users.noreply.github.com>
2022-11-03 23:03:16 +0100

    multiple support get post

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,49 @@ router.get("/", function (req, res, next) {
   res.json({ status: 200 });
 });
 
+router.get("/translate", async function (req, res, next) {
+  let from = req.body.from.toLowerCase();
+  let to = req.body.to.toLowerCase();
+  let text = req.body.text.toLowerCase();
+
+  const AVAILABLE_FROM = ["fr", "lis"];
+  const AVAILABLE_TO = ["fr", "lis"];
+
+  if (!AVAILABLE_FROM.includes(from)) {
+    res.json({
+      status: 404,
+      response:
+        "From : " +
+        from +
+        " is not an available language. Please use one of : " +
+        AVAILABLE_FROM.join(", "),
+    });
+  }
+
+  if (!AVAILABLE_TO.includes(to)) {
+    res.json({
+      status: 404,
+      response:
+        "To : " +
+        from +
+        " is not an available language. Please use one of : " +
+        AVAILABLE_FROM.join(", "),
+    });
+  }
+
+  let translatedText = await translateString(from, to, text);
+
+  if (!translatedText) {
+    res.json({
+      status: 404,
+      response: "Error during translation processing",
+    });
+    return;
+  }
+
+  res.json({ status: 200, response: translatedText });
+});
+
 router.post("/translate", async function (req, res, next) {
   let from = req.body.from.toLowerCase();
   let to = req.body.to.toLowerCase();
